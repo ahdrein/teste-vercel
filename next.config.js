92c038910fc4b6f16a1cdea45977d6fc2abc4be9
Andrews Henrique Drein <ahdrein@users.noreply.github.com>
2020-12-31 14:16:47 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
       locales: ['en-US', 'pt-BR'],
       // This is the default locale you want to be used when visiting
       // a non-locale prefixed path e.g. `/hello`
-      defaultLocale: 'en-US',
+      defaultLocale: 'pt-BR',
       // This is a list of locale domains and the default locale they
       // should handle (these are only required when setting up domain routing)
       domains: [
