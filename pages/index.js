593f4a4e9861481fcda73c9c0b2ef7e0047d065a
Andrews Henrique Drein <ahdrein@users.noreply.github.com>
2020-12-31 14:12:19 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import Link from 'next/link';
 function Home(){
     return <div>
             <h1>Home</h1>
-            <Link href="/sobre">
-                <a >Sobre</a>
+            <Link href="/sobre" locale="pt-BR">
+                <a >Sobre BR</a>
+            </Link>
+    
+            <Link href="/sobre" locale="en-US">
+                <a >Sobre US</a>
             </Link>
         </div>
 }
